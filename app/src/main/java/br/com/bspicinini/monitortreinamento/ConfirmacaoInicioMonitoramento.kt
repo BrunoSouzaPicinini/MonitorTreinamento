61502fc6fed4ed2ac7729a117deabc483c469c0b
Bruno de Souza Picinini <brunosouzapicinini@gmail.com>
2020-11-08 17:25:10 -0300

    [2] adequacao de cores

---
+++ app/src/main/java/br/com/bspicinini/monitortreinamento/ConfirmacaoInicioMonitoramento.kt
@@ -0,0 +1,11 @@
+package br.com.bspicinini.monitortreinamento
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfirmacaoInicioMonitoramento : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirmacao_inicio_monitoramento)
+    }
+}
